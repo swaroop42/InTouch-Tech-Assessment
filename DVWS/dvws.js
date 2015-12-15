584558662862594ef891afdefa0b1d04ac9801d7
Swaroop <ssr@track24.com>
2015-12-14 21:59:23 -0500

    no message

--- DVWS/dvws.js
+++ DVWS/dvws.js
@@ -1,4 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+require('./lib/routes')(router);
 
 console.log('Data Visualization Web Service (DVWS) Has Started');
 
 
+module.exports = router;
